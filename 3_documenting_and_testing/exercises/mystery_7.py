621e73d18de41ec72b63f10abb2e79b8e73f469f
Sukhrob Muborakshoev <suhrob.m89@gmail.com>
2024-12-07 15:22:56 -0800

    Added docstring for exercises mystery_7, mystery_8 and mystery_9 in 3_documenting_and_testing.

--- 3_documenting_and_testing/exercises/mystery_7.py
+++ 3_documenting_and_testing/exercises/mystery_7.py
@@ -1,4 +1,25 @@
 def mystery_7(a, b):
+    """Filters through a list of strings, to return only those that contain a specific substring.
+
+    Parameters:
+        a (list of strings): A list of strings to search through
+        b (string): The substring to look for within each string in "a".
+
+    Returns:
+        list of strings: A list of strings from "a", that contain the substring "b".
+        
+        >>> mystery_7(["apple", "banana", "cherry"], "a")
+        ['apple', 'banana']
+
+        >>> mystery_7(["hello", "world", "python"], "o")
+        ['hello', 'world', 'python']
+
+        >>> mystery_7(["test", "case", "example"], "z")
+        []
+
+        >>> mystery_7(["cat", "bat", "rat"], "at")
+        ['cat', 'bat', 'rat']
+    """
     c = []
     for d in a:
         if b in d:
--- 3_documenting_and_testing/exercises/mystery_9.py
+++ 3_documenting_and_testing/exercises/mystery_9.py
@@ -1,4 +1,26 @@
 def mystery_9(a):
+    """Sorts a list of numbers in ascending order using the Bubble Sort algorithm.
+
+    Parameters:
+        a (list of int or float): The list of numbers to be sorted.
+
+    Returns:
+        list of int or float: The sorted list in ascending order.
+        
+        >>> mystery_9([5, 3, 8, 4, 2])
+        [2, 3, 4, 5, 8]
+
+        >>> mystery_9([10, -1, 0, 3, 2])
+        [-1, 0, 2, 3, 10]
+
+        >>> mystery_9([1, 2, 3, 4, 5])
+        [1, 2, 3, 4, 5]
+
+        >>> mystery_9([3])
+        [3]
+        >>> mystery_9([])
+        []
+    """
     b = len(a)
     for c in range(b):
         for d in range(0, b - c - 1):
