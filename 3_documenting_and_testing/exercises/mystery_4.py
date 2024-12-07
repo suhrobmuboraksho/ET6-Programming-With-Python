8d46c4d3e2a8b507ecf6335574b9ac2a88cfe0c2
Sukhrob Muborakshoev <suhrob.m89@gmail.com>
2024-12-07 13:50:48 -0800

    Added docstring for exercises mystery_3 and mystery_4 in 3_documenting_and_testing.

--- 3_documenting_and_testing/exercises/mystery_4.py
+++ 3_documenting_and_testing/exercises/mystery_4.py
@@ -1,4 +1,27 @@
+"""
+This module contains utility function, for generating sequential list up to a specified number.
+
+- `mystery_4(a)`: Returns a list starting from 0 up to provided integer as an input.
+"""
 def mystery_4(a):
+    """
+    Returns a list of integers starting from 0 up to a specified length.
+
+    Parameters:
+        a (int): The length of the list. Must be a non-negative integer.
+
+    Returns:
+        list: A list of integers starting from 0 up to (a-1), with a total length of `a`.
+
+        >>> mystery_4(0)
+        []
+
+        >>> mystery_4(5)
+        [0, 1, 2, 3, 4]
+
+        >>> mystery_4(10)
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    """
     b = []
 
     c = 0
