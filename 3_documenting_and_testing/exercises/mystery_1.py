e66b397c4358204ef2c10d311fa0a873e550303f
Sukhrob Muborakshoev <suhrob.m89@gmail.com>
2024-12-07 12:32:11 -0800

    Added docstring for exercises mystery_1 and mystery_2 in 3_documenting_and_testing.

--- 3_documenting_and_testing/exercises/mystery_1.py
+++ 3_documenting_and_testing/exercises/mystery_1.py
@@ -1,2 +1,30 @@
+"""
+This module contains utility function for basic mathematical operations.
+
+Currently, it includes:
+- `mystery_1`: A function to calculate the sum of two non-negative integers.
+"""
+
 def mystery_1(a,b):
+    """Returns the sum of two numbers.
+
+    Parameters:
+        a (int): greater than or equal to 0
+        b (int): greater than or equal to 0
+
+    Returns:
+        int: returns the sum of integers a and b
+        
+        >>> mystery_1(0,0)
+        0
+        
+        >>> mystery_1(5,5)
+        10
+        
+        >>> mystery_1(200,1)
+        201
+        
+        >>> mystery_1(35,55)
+        90
+    """
     return a + b
--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,4 +1,31 @@
+"""
+This module contains utility function for working with string.
+
+- `mystery_2`: A function to calculate length of a string if it's not empty and None if empty.
+"""
+
 def mystery_2(a):
+    """Returns the length of the string if it is non-empty and returns None if empty.
+
+    Parameters:
+        a (string): The input string
+
+    Returns:
+        int: The length of the string.
+        None: If the string is empty.
+        
+    >>> mystery_2("hello")
+    5
+    
+    >>> mystery_2("Python")
+    6
+    
+    >>> mystery_2("MIT")
+    3
+    
+    >>> mystery_2("")
+    
+    """ 
     if len(a) == 0:
         return None
 
