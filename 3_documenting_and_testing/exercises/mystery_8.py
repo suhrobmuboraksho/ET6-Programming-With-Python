98d74c6f2be7356c68508f4da89e01a08d84adec
Sukhrob Muborakshoev <suhrob.m89@gmail.com>
2024-12-07 15:22:56 -0800

    Added docstring for exercises mystery_7, mystery_8 and mystery_9 in 3_documenting_and_testing.

--- 3_documenting_and_testing/exercises/mystery_8.py
+++ 3_documenting_and_testing/exercises/mystery_8.py
@@ -1,4 +1,31 @@
 def mystery_8(a, b):
+    """Filters a list of strings to include only those, that contain a specific substring.
+
+    Parameters:
+        a (list of strings): A list to search through
+        b (string): The substring to search for
+
+    Returns:
+        list of strings: A list of strings from "a", that contain the substring "b".
+        
+        >>> mystery_8(["dog", "cat", "bird", "fish"], "i")
+        ['bird', 'fish']
+
+        >>> mystery_8(["tree", "bush", "flower"], "ee")
+        ['tree']
+
+        >>> mystery_8(["car", "cart", "cartoon"], "car")
+        ['car', 'cart', 'cartoon']
+
+        >>> mystery_8(["single"], "s")
+        ['single']
+    
+        >>> mystery_8(["red", "green", "blue"], "yellow")
+        []
+
+        >>> mystery_8([], "any")
+        []
+    """
     c = []
     while a:
         if b in a[0]:
