59cedf178c6aff2cf90103eff8d3c6bb96f794be
Sukhrob Muborakshoev <suhrob.m89@gmail.com>
2024-12-07 14:19:38 -0800

    Added docstring for exercises mystery_5 and mystery_6 in 3_documenting_and_testing.

--- 3_documenting_and_testing/exercises/mystery_6.py
+++ 3_documenting_and_testing/exercises/mystery_6.py
@@ -1,4 +1,22 @@
 def mystery_6(a, b):
+    """Generates and returns a list of sequential integers starting from a given number.
+
+    Parameters:
+        a (int): The number of elements to generate. Must not be negative.
+        b (int): The starting number for the sequence.
+
+    Returns:
+        list: A list of "a" integers starting from "b"
+        
+        >>> mystery_6(3, 10)
+        [10, 11, 12]
+
+        >>> mystery_6(5, 0)
+        [0, 1, 2, 3, 4]
+        
+        >>> mystery_6(0, 5)
+        []
+    """
     if a == 0:
         return []
 
