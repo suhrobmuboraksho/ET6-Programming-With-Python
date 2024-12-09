bd28298ecc474395e82d7b2ec5598ccb674bea17
Sukhrob Muborakshoev <suhrob.m89@gmail.com>
2024-12-09 08:18:28 -0800

    Added type annotation and unittest for mystery_1, mystery_2.

--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -1,6 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Created on 12 09 24
+    
+    @author: Sukhrob Muborakshoev
+"""
+
 import unittest
 
 from ..mystery_1 import mystery_1
 
 class TestMystery1(unittest.TestCase):
-    """ """
+    """Test the mystery_1 function"""
+    
+    def test_0(self):
+        """It should evaluate 5, 5 to 10"""
+        actual = mystery_1(5,5)
+        expected = 10
+        self.assertEqual(actual, expected)
+    
+    def test_1(self):
+        """It should evaluate 200, 1 to 201"""
+        actual = mystery_1(200,1)
+        expected = 201
+        self.assertEqual(actual, expected)
+        
+    def test_2(self):
+        """It should evaluate 35, 55 to 90"""
+        actual = mystery_1(35,55)
+        expected = 90
+        self.assertEqual(actual, expected)
+        
+    def test_3(self):
+        """It should evaluate 0, 0 to 0"""
+        actual = mystery_1(0,0)
+        expected=0
+        self.assertEqual(actual, expected)
+    
--- 3_documenting_and_testing/exercises/tests/test_mystery_2.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_2.py
@@ -1,6 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Created on 12 09 24
+    
+    @author: Sukhrob Muborakshoev
+"""
 import unittest
 
 from ..mystery_2 import mystery_2
 
 class TestMystery2(unittest.TestCase):
-    """ """
+    """Test the mystery_2 function"""
+    def test_0(self):
+        """ It should evaluate "hello" to 5 """
+        actual = mystery_2("hello")
+        expected = 5
+        self.assertEqual(actual,expected)
+    
+    def test_1(self):
+        """It should evaluate "Python" to 6 """
+        actual = mystery_2("Python")
+        expected = 6
+        self.assertEqual(actual,expected)
+        
+    def test_2(self):
+        """It should evaluate "MIT" to 3 """
+        actual = mystery_2("MIT")
+        expected = 3
+        self.assertEqual(actual, expected)
+        
+    def test_3(self):
+        """It should evaluate empty string to None"""
+        actual = mystery_2("")
+        expected = None
+        self.assertEqual(actual,expected)
