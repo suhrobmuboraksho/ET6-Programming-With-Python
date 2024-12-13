b8bc9550313fa3315a4322e970a44f690297aa78
Sukhrob Muborakshoev <suhrob.m89@gmail.com>
2024-12-12 16:14:43 -0800

    Modified mystery_1 and mystery_2 unittests.

--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -12,4 +12,27 @@ import unittest
 from ..mystery_1 import mystery_1
 
 class TestMystery1(unittest.TestCase):
-    """ """
+    """Test the mystery_1 function"""
+    def test_0(self):
+        """It should evaluate to 0"""
+        actual = mystery_1(0, 0)
+        expected = 0
+        self.assertEqual(actual,expected)
+        
+    def test_1(self):
+        """It should evaluate to 10"""
+        actual = mystery_1(5,5)
+        expected = 10
+        self.assertEqual(actual, expected)
+    
+    def test_2(self):
+        """It should evaluate to 201"""
+        actual = mystery_1(200, 1)
+        expected = 201
+        self.assertEqual(actual, expected)
+    
+    def test_3(self):
+        """It should evaluate to 90"""
+        actual = mystery_1(35, 55)
+        expected = 90
+        self.assertEqual(actual, expected)
--- 3_documenting_and_testing/exercises/tests/test_mystery_2.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_2.py
@@ -11,4 +11,28 @@ import unittest
 from ..mystery_2 import mystery_2
 
 class TestMystery2(unittest.TestCase):
-    """ """
+    """Test the mystery_2 function"""
+    
+    def test_0(self):
+        """It should evaluate to 5"""
+        actual = mystery_2("hello")
+        expected = 5
+        self.assertEqual(actual, expected)
+        
+    def test_1(self):
+        """It should evaluate to 6"""
+        actual = mystery_2("Python")
+        expected = 6
+        self.assertEqual(actual, expected)
+        
+    def test_2(self):
+        """It should evaluate to 3"""
+        actual = mystery_2("MIT")
+        expected = 3
+        self.assertEqual(actual, expected)
+        
+    def test_3(self):
+        """It should evaluate to None"""
+        actual = mystery_2("")
+        expected = None
+        self.assertEqual(actual, expected)
